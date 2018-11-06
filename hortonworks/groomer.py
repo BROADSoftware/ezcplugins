89c2abdeae3725bba21efb0a4a70f81a49343837
Serge ALEXANDRE <sa@broadsoftware.com>
2018-11-06 11:00:48 +0100

    Hortonworks plugin: Added oraclejdk support

--- hortonworks/groomer.py
+++ hortonworks/groomer.py
@@ -18,29 +18,61 @@
 import os
 from misc import ERROR, setDefaultInMap, appendPath,lookupRepository
 
+HORTONWORKS = "hortonworks"
+CLUSTER = "cluster"
+DISABLED = "disabled"
+CONFIG = "config"
+ANSIBLE_REPO_FOLDER = "ansible_repo_folder"
+DATA = "data"
+CONFIG_FILE = "configFile"
+ROLE_PATHS = "rolePaths"
+EXTRA_GROUP_BY_NAME = "extraGroupByName"
+HW_SERVICES = "hw_services"
+NAME = "name"
+NODES = "nodes"
+ZOOKEEPERS = "zookeepers"
+GROUP_BY_NAME = "groupByName"
+KAFKA_BROKERS = "kafka_brokers"
+ROLES="roles"
+JAVA="java"
+EMBEDDED="embedded"
+ORACLEJDK="oraclejdk"
+ORACLEJDK_TARBALL_LOCATION="oraclejdk_tarball_location"
+ORACLEJDK_JCE_LOCATION="oraclejdk_jce_location"
+REPOSITORIES="repositories"
+
+
+
 def groom(plugin, model):
-    setDefaultInMap(model["cluster"]["hortonworks"], "disabled", False)
-    if model["cluster"]["hortonworks"]["disabled"]:
+    setDefaultInMap(model[CLUSTER][HORTONWORKS], DISABLED, False)
+    if model[CLUSTER][HORTONWORKS][DISABLED]:
         return False
     else:
-        lookupRepository(model, "hortonworks") 
-        if "hortonworks" not in model["config"] or "ansible_repo_folder" not in model["config"]["hortonworks"]:
+        lookupRepository(model, HORTONWORKS) 
+        if HORTONWORKS not in model[CONFIG] or ANSIBLE_REPO_FOLDER not in model[CONFIG][HORTONWORKS]:
             ERROR("Missing 'hortonworks.ansible_repo_folder' in configuration file")
-        ansible_repo_folder = appendPath(os.path.dirname(model["data"]["configFile"]),  model["config"]["hortonworks"]["ansible_repo_folder"]) 
-        model["config"]["hortonworks"]["ansible_repo_folder"] = ansible_repo_folder
-        model["data"]["rolePaths"].add(appendPath(ansible_repo_folder, "roles"))
-        # We need to define some groups for the intention of external tools.
+        ansible_repo_folder = appendPath(os.path.dirname(model[DATA][CONFIG_FILE]),  model[CONFIG][HORTONWORKS][ANSIBLE_REPO_FOLDER]) 
+        model[CONFIG][HORTONWORKS][ANSIBLE_REPO_FOLDER] = ansible_repo_folder
+        model[DATA][ROLE_PATHS].add(appendPath(ansible_repo_folder, "roles"))
+        # ------------- We need to define some groups for the intention of external tools.
         zookeepers = [] 
         kafka_brokers = []
-        model["data"]["extraGroupByName"] = {}
-        for role in model["cluster"]["roles"]:
-            if "hw_services" in role:
-                if "ZOOKEEPER_SERVER" in role["hw_services"]:
-                    zookeepers.extend(map(lambda x : x["name"], role["nodes"]))
-                if "KAFKA_BROKER" in role["hw_services"]:
-                    kafka_brokers.extend(map(lambda x : x["name"], role["nodes"]))
-        if "zookeepers" not in model["data"]["groupByName"]:
-            model["data"]["extraGroupByName"]["zookeepers"] = zookeepers
-        if "kafka_brokers" not in model["data"]["groupByName"]:
-            model["data"]["extraGroupByName"]["kafka_brokers"] = kafka_brokers
+        model[DATA][EXTRA_GROUP_BY_NAME] = {}
+        for role in model[CLUSTER][ROLES]:
+            if HW_SERVICES in role:
+                if "ZOOKEEPER_SERVER" in role[HW_SERVICES]:
+                    zookeepers.extend(map(lambda x : x[NAME], role[NODES]))
+                if "KAFKA_BROKER" in role[HW_SERVICES]:
+                    kafka_brokers.extend(map(lambda x : x[NAME], role[NODES]))
+        if ZOOKEEPERS not in model[DATA][GROUP_BY_NAME]:
+            model[DATA][EXTRA_GROUP_BY_NAME][ZOOKEEPERS] = zookeepers
+        if KAFKA_BROKERS not in model[DATA][GROUP_BY_NAME]:
+            model[DATA][EXTRA_GROUP_BY_NAME][KAFKA_BROKERS] = kafka_brokers
+        # ---------------------------- Handle java
+        setDefaultInMap(model[CLUSTER][HORTONWORKS], JAVA, EMBEDDED)
+        if model[CLUSTER][HORTONWORKS][JAVA] == ORACLEJDK:
+            if ORACLEJDK_TARBALL_LOCATION not in model[DATA][REPOSITORIES][HORTONWORKS]:
+                ERROR("'hortonworks.java' is set to 'oraclejdk' while there is no 'repositories.hortonworks.oraclejdk_tarball_location' defined in configuration file!")
+            if ORACLEJDK_JCE_LOCATION not in model[DATA][REPOSITORIES][HORTONWORKS]:
+                ERROR("'hortonworks.java' is set to 'oraclejdk' while there is no 'repositories.hortonworks.oraclejdk_jce_location' defined in configuration file!")
         return True
