53927721f12f4c7ca0a8497fd0612f37726c7f76
Serge ALEXANDRE <sa@broadsoftware.com>
2018-11-25 12:47:22 +0100

    Added user/database handling for postgresql_server

--- postgresql_server/groomer.py
+++ postgresql_server/groomer.py
@@ -15,24 +15,38 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with EzCluster.  If not, see <http://www.gnu.org/licenses/lgpl-3.0.html>.
 
+import hashlib
 from misc import ERROR,lookupRepository,setDefaultInMap
 
 POSTGRESQL_SERVER = "postgresql_server"
 CLUSTER = "cluster"
 DISABLED = "disabled"
 PASSWORD = "password"
-ENCRYPTED_PASSWORD = "encrypted_password"
+USERS = "users"
+DATABASES = "databases"
+NAME="name"
 
 def groom(plugin, model):
     setDefaultInMap(model[CLUSTER][POSTGRESQL_SERVER], DISABLED, False)
     if model[CLUSTER][POSTGRESQL_SERVER][DISABLED]:
         return False
     else:
+        setDefaultInMap(model[CLUSTER][POSTGRESQL_SERVER], USERS, [])
+        setDefaultInMap(model[CLUSTER][POSTGRESQL_SERVER], DATABASES, [])       # To ease templates
         lookupRepository(model, POSTGRESQL_SERVER)
+        unencryptedCount = 0
         if PASSWORD in model[CLUSTER][POSTGRESQL_SERVER]:
-            if ENCRYPTED_PASSWORD in model[CLUSTER][POSTGRESQL_SERVER]:
-                ERROR("posgresql_server: 'password' and 'encrypted_password' can't be both defined!")
-            else:
-                print("\n**WARNING**: usage of clear text password is discouraged. Use 'encrypted_password' instead.")
-                print("To encrypt a password: python -c \"import hashlib; print hashlib.md5('yourPassword'+'yourUser').hexdigest()\"\n")
+            if not model[CLUSTER][POSTGRESQL_SERVER][PASSWORD].startswith("md5"):
+                unencryptedCount += 1
+                # Will encrypt password, as unencrypted ones may be unsupported in future postgresl releases
+                model[CLUSTER][POSTGRESQL_SERVER][PASSWORD] = "md5" + hashlib.md5(model[CLUSTER][POSTGRESQL_SERVER][PASSWORD] + 'postgres').hexdigest()
+        for user in model[CLUSTER][POSTGRESQL_SERVER][USERS]:
+            if not user[PASSWORD].startswith("md5"):
+                unencryptedCount += 1
+                user[PASSWORD] = "md5" + hashlib.md5(user[PASSWORD] + user[NAME]).hexdigest()
+        if unencryptedCount > 0:
+            print("")
+            print("**WARNING**: usage of clear text password is discouraged.")
+            print("To encrypt a password: \n\tpython -c \"import hashlib; print 'md5' + hashlib.md5('yourPassword'+'yourUser').hexdigest()\"")
+            print("And set result in place of clear text password.")
         return True
