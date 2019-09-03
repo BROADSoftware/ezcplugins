d427f148a25623f1cd5abfa20e8d4619eeaab3d8
Serge ALEXANDRE <sa@broadsoftware.com>
2019-09-03 11:42:16 +0200

    Add docker_nexus_proxy and docker_registry_hack

---
+++ docker_registry_hack/groomer.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2018 BROADSoftware
+#
+# This file is part of EzCluster
+#
+# EzCluster is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# EzCluster is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with EzCluster.  If not, see <http://www.gnu.org/licenses/lgpl-3.0.html>.
+
+
+from misc import setDefaultInMap,resolveDns
+
+def groom(_plugin, model):
+    setDefaultInMap(model["cluster"]["docker_registry_hack"], "disabled", False)
+    if model["cluster"]["docker_registry_hack"]["disabled"]:
+        return False
+    else:
+        if 'etc_hosts_entries' in model["cluster"]["docker_registry_hack"]:
+            for entry in model["cluster"]["docker_registry_hack"]["etc_hosts_entries"]:
+                entry["ip"] = resolveDns(entry["ip"])
+        return True
+    
