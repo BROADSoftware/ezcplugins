ec6cebee57b49f622a9f2254739bc24f83d00db6
Serge ALEXANDRE <sa@broadsoftware.com>
2020-01-24 16:29:27 +0100

    Adjustment for Kubespray V2.12.0 PodSecurityPolicy now ON by default

--- k8s/kubespray/groomer.py
+++ k8s/kubespray/groomer.py
@@ -43,7 +43,7 @@ def groom(_plugin, model):
     setDefaultInMap(model[CLUSTER][K8S][KUBESPRAY], "basic_auth", False)
     setDefaultInMap(model[CLUSTER][K8S][KUBESPRAY], METRICS_SERVER, True)
     setDefaultInMap(model[CLUSTER][K8S][KUBESPRAY], AUDIT, False)
-    setDefaultInMap(model[CLUSTER][K8S][KUBESPRAY], POD_SECURITY_POLICIES, False)
+    setDefaultInMap(model[CLUSTER][K8S][KUBESPRAY], POD_SECURITY_POLICIES, True)
     if model[CLUSTER][K8S][KUBESPRAY][DISABLED]:
         return False
     else:
