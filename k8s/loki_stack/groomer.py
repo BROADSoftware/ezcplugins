97665f52c44dabb092a643f4e9a39a320f6fab8c
Serge ALEXANDRE <sa@broadsoftware.com>
2020-03-02 15:03:55 +0100

    Update loki_stack to add logcli support

--- k8s/loki_stack/groomer.py
+++ k8s/loki_stack/groomer.py
@@ -53,47 +53,51 @@ def groom(_plugin, model):
         setDefaultInMap(model[CLUSTER][K8S][LOKI_STACK][LOKI], DISABLED, False)
         if not model[CLUSTER][K8S][LOKI_STACK][LOKI][DISABLED]:
             if PERSISTENCE not in model[CLUSTER][K8S][LOKI_STACK][LOKI]:
-                ERROR("A loki.persistence map must be defined if loki is not disabled.")
+                ERROR("A 'loki.persistence map' must be defined if 'loki' is not disabled.")
             setDefaultInMap(model[CLUSTER][K8S][LOKI_STACK][LOKI][PERSISTENCE], DISABLED, False)
             if not model[CLUSTER][K8S][LOKI_STACK][LOKI][PERSISTENCE][DISABLED]:
                 if STORAGE_CLASS not in model[CLUSTER][K8S][LOKI_STACK][LOKI][PERSISTENCE]:
-                    ERROR("A storage_class must be defined if persistence is enabled on loki")
+                    ERROR("A 'storage_class' must be defined if 'persistence' is enabled on 'loki'")
                 if SIZE_GB not in model[CLUSTER][K8S][LOKI_STACK][LOKI][PERSISTENCE]:
-                    ERROR("A size_gb must be defined if persistence is enabled on loki")
+                    ERROR("A 'size_gb' must be defined if 'persistence' is enabled on 'loki'")
         # -------------- grafana parameters checking
         setDefaultInMap(model[CLUSTER][K8S][LOKI_STACK][GRAFANA], DISABLED, False)
         if not model[CLUSTER][K8S][LOKI_STACK][GRAFANA][DISABLED]:
             if PERSISTENCE not in model[CLUSTER][K8S][LOKI_STACK][GRAFANA]:
-                ERROR("A grafana.persistence map must be defined if grafana is not disabled.")
+                ERROR("A 'grafana.persistence' map must be defined if 'grafana' is not disabled.")
             setDefaultInMap(model[CLUSTER][K8S][LOKI_STACK][GRAFANA][PERSISTENCE], DISABLED, False)
             if not model[CLUSTER][K8S][LOKI_STACK][GRAFANA][PERSISTENCE][DISABLED]:
                 if STORAGE_CLASS not in model[CLUSTER][K8S][LOKI_STACK][GRAFANA][PERSISTENCE]:
-                    ERROR("A storage_class must be defined if persistence is enabled on loki.grafana")
+                    ERROR("A 'storage_class' must be defined if 'persistence' is enabled on 'loki.grafana'")
                 if SIZE_GB not in model[CLUSTER][K8S][LOKI_STACK][GRAFANA][PERSISTENCE]:
-                    ERROR("A size_gb must be defined if grafana.persistence is enabled on loki.grafana")
+                    ERROR("A 'size_gb' must be defined if 'grafana.persistence' is enabled on 'loki.grafana'")
         # -------------- prometheus parameters checking
         setDefaultInMap(model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS], DISABLED, False)
         if not model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][DISABLED]:
+            if SERVER not in model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS]:
+                ERROR("A 'server' map must be defined if 'loki.prometheus' is defined")
             setDefaultInMap(model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][SERVER], DISABLED, False)
             if not model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][SERVER][DISABLED]:
                 if PERSISTENCE not in model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][SERVER]:
-                    ERROR("A prometheus.server.persistence map must be defined if loki.prometheus.server is not disabled.")
+                    ERROR("A 'loki.prometheus.server.persistence' map must be defined if 'loki.prometheus.server' is not disabled.")
                 setDefaultInMap(model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][SERVER][PERSISTENCE], DISABLED, False)
                 if not model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][SERVER][PERSISTENCE][DISABLED]:
                     if STORAGE_CLASS not in model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][SERVER][PERSISTENCE]:
-                        ERROR("A storage_class must be defined if persistence is enabled on loki.prometheus.server")
+                        ERROR("A 'storage_class' must be defined if 'persistence' is enabled on 'loki.prometheus.server'")
                     if SIZE_GB not in model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][SERVER][PERSISTENCE]:
-                        ERROR("A size_gb must be defined if grafana.persistence is enabled on loki.prometheus.server")
+                        ERROR("A 'size_gb' must be defined if 'persistence' is enabled on 'loki.prometheus.server'")
+            if ALERT_MANAGER not in model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS]:
+                ERROR("An 'alert_manager' map must be defined if 'loki.prometheus' is defined")
             setDefaultInMap(model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][ALERT_MANAGER], DISABLED, False)
             if not model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][ALERT_MANAGER][DISABLED]:
                 if PERSISTENCE not in model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][ALERT_MANAGER]:
-                    ERROR("A prometheus.alert_manager.persistence map must be defined if loki.prometheus.alert_manager is not disabled.")
+                    ERROR("A 'loki.prometheus.alert_manager.persistence' map must be defined if 'loki.prometheus.alert_manager' is not disabled.")
                 setDefaultInMap(model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][ALERT_MANAGER][PERSISTENCE], DISABLED, False)
                 if not model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][ALERT_MANAGER][PERSISTENCE][DISABLED]:
                     if STORAGE_CLASS not in model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][ALERT_MANAGER][PERSISTENCE]:
-                        ERROR("A storage_class must be defined if persistence is enabled on loki.prometheus.alert_manager")
+                        ERROR("A 'storage_class' must be defined if 'persistence' is enabled on 'loki.prometheus.alert_manager'")
                     if SIZE_GB not in model[CLUSTER][K8S][LOKI_STACK][PROMETHEUS][ALERT_MANAGER][PERSISTENCE]:
-                        ERROR("A size_gb must be defined if grafana.persistence is enabled on loki.prometheus.alert_manager")
+                        ERROR("A 'size_gb' must be defined if 'persistence' is enabled on 'loki.prometheus.alert_manager'")
         return True
 
 def groom2(_plugin, model):
@@ -111,6 +115,21 @@ def groom2(_plugin, model):
                 lbAddressInRange = True
         if not lbAddressInRange:
             ERROR("grafana.lb_address is not included in one of metallb.external_ip_ranges")
+
+    if LB_ADDRESS in model[CLUSTER][K8S][LOKI_STACK][LOKI]:
+        if METALLB not in model[CLUSTER][K8S] or model[CLUSTER][K8S][METALLB][DISABLED]:
+            ERROR("A lb_address is defined while there is no metallb plugin")
+        model[CLUSTER][K8S][LOKI_STACK][LOKI][LB_ADDRESS] = resolveDnsAndCheck(model[CLUSTER][K8S][LOKI_STACK][LOKI][LB_ADDRESS])
+        lb_address =  ipaddress.ip_address(u"" + model[CLUSTER][K8S][LOKI_STACK][LOKI][LB_ADDRESS])
+
+        lbAddressInRange = False
+        for rangeip in model[CLUSTER][K8S][METALLB][EXTERNAL_IP_RANGES]:
+            first_ip = ipaddress.ip_address(u"" + rangeip[FIRST])
+            last_ip = ipaddress.ip_address(u"" + rangeip[LAST])
+            if lb_address >= first_ip and lb_address <= last_ip:
+                lbAddressInRange = True
+        if not lbAddressInRange:
+            ERROR("loki.lb_address is not included in one of metallb.external_ip_ranges")
 
         
     
